6c4d0c7efb2c75ad1a87f43a9c455bcad1c662db
Devendra <sutards777@gmail.com>
2023-03-28 14:40:09 +0530

    Java Programs

---
+++ GreaterNumberExample.java
@@ -0,0 +1,20 @@
+package Core.Java;
+
+public class GreaterNumberExample {
+
+	public static void main(String[] args) {
+		
+		int value1=20, value2=20;
+		
+		if(value1>value2)
+		{
+			System.out.println(value1);
+		}
+		else
+		{
+			System.out.println(value2);
+		}
+
+	}
+
+}
---
+++ TestExample.java
@@ -0,0 +1,12 @@
+package Core.Java;
+
+public class TestExample{
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Hello World!!");
+
+	}
+
+}
