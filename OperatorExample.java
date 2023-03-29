7f52d9614658f7b3d7caafc06bf6138db315219a
Devendra <sutards777@gmail.com>
2023-03-29 13:23:34 +0530

    operator example added

---
+++ OperatorExample.java
@@ -0,0 +1,44 @@
+package Core.Java;
+
+
+public class OperatorExample {
+
+	public static void main(String[] args) {
+		
+		int a=10,b=20;
+		
+		int result=(a<b)?a:b;
+		
+		System.out.println(result);
+		
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(b%a);
+		
+		
+		System.out.println(a>b);
+		System.out.println(a<b);
+		System.out.println(a>=b);
+		System.out.println(a<=b);
+		System.out.println(a!=b);
+		System.out.println(a==b);
+		
+		
+		System.out.println((a!=b)&&(a>b));
+		System.out.println((a!=b)||(a>b));
+		
+		
+		System.out.println(a>>2);
+		System.out.println(b<<2);
+		
+		a=a+10;
+		a+=10;
+		
+		a=a*b;
+		a*=b;
+	}
+
+}
